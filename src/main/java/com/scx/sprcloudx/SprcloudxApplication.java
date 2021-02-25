c65bff20427d564787ea18e211156264bbda237b
bin.qiu <nigelccfate@163.com>
2021-02-25 11:25:43 +0800

    test

---
+++ src/main/java/com/scx/sprcloudx/SprcloudxApplication.java
@@ -0,0 +1,13 @@
+package com.scx.sprcloudx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprcloudxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprcloudxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scx/sprcloudx/SprcloudxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scx.sprcloudx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprcloudxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
